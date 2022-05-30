efad6899a424e0f32d344921813e7b1617a36fc0
RnEzequiel <ezerenolfi@gmail.com>
2022-05-30 09:13:07 -0300

    Agregados tests de los ejercicios 1 y 2.

---
+++ tests/test_ejercicio_2_RnEzequiel.py
@@ -0,0 +1,29 @@
+"""
+Pruebas ejercicio 2.
+"""
+
+from src.ejercicio2 import signo
+
+def test_signo_positivo():
+    """
+    Prueba la devolución del valor "1" ante un número positivo.
+    """
+    resultado = signo(4)
+    assert isinstance(resultado, int)
+    assert resultado == 1
+
+def test_signo_cero():
+    """
+    Prueba la devolución del valor "0" ante un valor de cero.
+    """
+    resultado = signo(0)
+    assert isinstance(resultado, int)
+    assert resultado == 0
+
+def test_signo_negativo():
+    """
+    Prueba la devolución del valor "-1" ante un número negativo.
+    """
+    resultado = signo(-8)
+    assert isinstance(resultado, int)
+    assert resultado == -1
